7b8d64bbddd9f28b1e9f5382d9d8502d0450d224
Nathen Harvey <nharvey@chef.io>
2016-11-28 17:03:23 -0800

    A simple web site...
    well, it's awesome.
    
    awesome in it's simplicity
    
    Signed-off-by: Nathen Harvey <nharvey@chef.io>

---
+++ cookbooks/simple_web/metadata.rb
@@ -0,0 +1,12 @@
+name 'simple_web'
+maintainer 'Joe Gardiner'
+maintainer_email 'joe@chef.io'
+license 'all_rights'
+description 'Installs/Configures simple_web'
+long_description 'Installs/Configures simple_web'
+version '0.1.0'
+
+depends 'apt'
+depends 'firewall'
+depends 'apache2'
+
